9518ba1f0962f658abc26fef95f8aa28d537b27e
Márcio Fábio Althmann <Althmann@macbook.local>
2014-08-21 21:02:46 -0300

    swift - constantes e variaveis

---
+++ variaveis-constantes.playground/section-1.swift
@@ -0,0 +1,13 @@
+
+let pi = 3.14
+
+var nome = "Márcio Fábio Althmann"
+var idade = 30
+
+var anoDeNascimento: Int
+anoDeNascimento = 1983
+
+var primeiroNome: String
+primeiroNome = "Márcio"
+
+var dia = 8, mes = 10, ano = 1983
