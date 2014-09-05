0f3c6111cdf6d2bac54dbcd0fa251df8edc4162a
Márcio Fábio Althmann <Althmann@macbook.local>
2014-09-04 23:22:13 -0300

    control-flow

---
+++ control-flow.playground/section-1.swift
@@ -0,0 +1,24 @@
+
+var somaDosPares = 0
+
+for i in 1...10{
+    println(i)
+}
+
+
+for _ in 1...10{
+    println("Olá")
+}
+
+var livros = ["A Sociedade do Anel", "As Duas Torres", "O Retorno do Rei"]
+
+for livro in livros{
+    println(livro);
+}
+
+
+for var i = 0; i < 10; ++i{
+    if(i % 2 == 0){
+        println(i);
+    }
+}
